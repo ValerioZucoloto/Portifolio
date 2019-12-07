a14537670cdfed46cbb9b3b3a7712da9a90310dc
ValerioZucoloto <55113492+ValerioZucoloto@users.noreply.github.com>
2019-12-07 08:16:25 -0300

    Estrutura de repetição - Exercicio
    Nesse programa o usuário informa vários números até que a soma desses não ultrapasse o número 200.

--- main.cs
+++ main.cs
@@ -2,30 +2,21 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    int n1, n2, n3, pesoN1, pesoN2, pesoN3;
-   float media;
+    int x, soma=0;
 
-   
-   Console.WriteLine("Informe a primeira nota: ");
-   n1 = int.Parse (Console.ReadLine());
-   Console.WriteLine ("Informe o peso da N1: ");
-   pesoN1 = int.Parse (Console.ReadLine());
+    Console.WriteLine("Entre com um número: ");
+    x = int.Parse (Console.ReadLine());
+    soma = soma + x;
+
+    while (soma < 200) {
+      Console.WriteLine ("Entre com um número: ");
+      x = int.Parse (Console.ReadLine());
+
+      soma = soma + x;
 
 
-   Console.WriteLine("Informe a segunda nota: ");
-   n2 = int.Parse (Console.ReadLine());
-   Console.WriteLine ("Informe o peso da N2: ");
-   pesoN2 = int.Parse (Console.ReadLine());
+    }
 
-   Console.WriteLine("Informe a terceira nota: ");
-   n3 = int.Parse (Console.ReadLine());
-   Console.WriteLine ("Informe o peso da N3: ");
-   pesoN3 = int.Parse (Console.ReadLine());
 
-   media = (n1*pesoN1 + n2*pesoN2 + n3*pesoN3);
-   media /= 100;
-
-   Console.WriteLine("A média ponderada das notas do aluno é: " + media.ToString("F2"));
-   
   }
 }
