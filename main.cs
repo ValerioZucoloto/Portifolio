5d2c4bdd19788cd8d81f66c7791a28234a85988a
ValerioZucoloto <55113492+ValerioZucoloto@users.noreply.github.com>
2019-12-07 07:51:48 -0300

    Exercício iteração com usuário
    Nesse exercicio foi solicitado ao usuário que informasse 3 notas e seus respectivos pesos, em seguida era realizado a média ponderada das notas.

--- main.cs
+++ main.cs
@@ -2,19 +2,30 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    
-    int num1= 0, num2= 0;
+    int n1, n2, n3, pesoN1, pesoN2, pesoN3;
+   float media;
 
-    Console.WriteLine ("Informe um número: ");
-    string entrada = Console.ReadLine();
-    num1 = Convert.ToInt32(entrada);
+   
+   Console.WriteLine("Informe a primeira nota: ");
+   n1 = int.Parse (Console.ReadLine());
+   Console.WriteLine ("Informe o peso da N1: ");
+   pesoN1 = int.Parse (Console.ReadLine());
 
-    Console.WriteLine ("Informe um número: ");
-    entrada = Console.ReadLine();
-    num2 = Convert.ToInt32(entrada);
 
-    int soma = num1 + num2;
-    Console.WriteLine ("Soma: {0}", soma);
+   Console.WriteLine("Informe a segunda nota: ");
+   n2 = int.Parse (Console.ReadLine());
+   Console.WriteLine ("Informe o peso da N2: ");
+   pesoN2 = int.Parse (Console.ReadLine());
 
+   Console.WriteLine("Informe a terceira nota: ");
+   n3 = int.Parse (Console.ReadLine());
+   Console.WriteLine ("Informe o peso da N3: ");
+   pesoN3 = int.Parse (Console.ReadLine());
+
+   media = (n1*pesoN1 + n2*pesoN2 + n3*pesoN3);
+   media /= 100;
+
+   Console.WriteLine("A média ponderada das notas do aluno é: " + media.ToString("F2"));
+   
   }
 }
