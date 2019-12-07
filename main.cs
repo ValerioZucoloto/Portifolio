c8d726448b2b0815d4ec28a0c5f7028da8ff60a5
ValerioZucoloto <55113492+ValerioZucoloto@users.noreply.github.com>
2019-12-07 08:09:30 -0300

    Estrutura de repeticao - Exercicio
    Nesse exercício, foi feito um programa que pedisse ao usuário que informasse letras aleatórias, após informar  o programa retornaria se a mesma era vogal ou não.

--- main.cs
+++ main.cs
@@ -2,30 +2,28 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    int n1, n2, n3, pesoN1, pesoN2, pesoN3;
-   float media;
+    char caractere = ' ';
+    bool ehVogal = false;
 
-   
-   Console.WriteLine("Informe a primeira nota: ");
-   n1 = int.Parse (Console.ReadLine());
-   Console.WriteLine ("Informe o peso da N1: ");
-   pesoN1 = int.Parse (Console.ReadLine());
+    while (ehVogal == false){
+      Console.WriteLine ("Entre com uma letra: ");
+      caractere = char.Parse (Console.ReadLine());
 
+      switch (char.ToUpper(caractere)) {
+        case 'A':
+        case 'E':
+        case 'I':
+        case 'O':
+        case 'U':
+          ehVogal = true;
+          break;
 
-   Console.WriteLine("Informe a segunda nota: ");
-   n2 = int.Parse (Console.ReadLine());
-   Console.WriteLine ("Informe o peso da N2: ");
-   pesoN2 = int.Parse (Console.ReadLine());
-
-   Console.WriteLine("Informe a terceira nota: ");
-   n3 = int.Parse (Console.ReadLine());
-   Console.WriteLine ("Informe o peso da N3: ");
-   pesoN3 = int.Parse (Console.ReadLine());
-
-   media = (n1*pesoN1 + n2*pesoN2 + n3*pesoN3);
-   media /= 100;
-
-   Console.WriteLine("A média ponderada das notas do aluno é: " + media.ToString("F2"));
-   
+        default:
+          ehVogal = false;
+          break;
+      }
+    }
+    Console.WriteLine ("Achei uma vogal: " + caractere);
+    Console.ReadKey();
   }
 }
