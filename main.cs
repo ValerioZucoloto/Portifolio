ca62261cd41a5edc17ca79e4a81231ea37ca6746
ValerioZucoloto <55113492+ValerioZucoloto@users.noreply.github.com>
2019-12-07 08:02:04 -0300

    Estrutura condicional - Exercicio

--- main.cs
+++ main.cs
@@ -2,30 +2,22 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    int n1, n2, n3, pesoN1, pesoN2, pesoN3;
-   float media;
+    int hora;
 
-   
-   Console.WriteLine("Informe a primeira nota: ");
-   n1 = int.Parse (Console.ReadLine());
-   Console.WriteLine ("Informe o peso da N1: ");
-   pesoN1 = int.Parse (Console.ReadLine());
+    Console.WriteLine ("Quantas horas ?");
+    hora= int.Parse (Console.ReadLine());
 
-
-   Console.WriteLine("Informe a segunda nota: ");
-   n2 = int.Parse (Console.ReadLine());
-   Console.WriteLine ("Informe o peso da N2: ");
-   pesoN2 = int.Parse (Console.ReadLine());
-
-   Console.WriteLine("Informe a terceira nota: ");
-   n3 = int.Parse (Console.ReadLine());
-   Console.WriteLine ("Informe o peso da N3: ");
-   pesoN3 = int.Parse (Console.ReadLine());
-
-   media = (n1*pesoN1 + n2*pesoN2 + n3*pesoN3);
-   media /= 100;
-
-   Console.WriteLine("A média ponderada das notas do aluno é: " + media.ToString("F2"));
-   
+    if (hora < 12) {
+      Console.WriteLine ("Bom dia !");
+    }
+    else{
+      if (hora < 18){
+        Console.WriteLine ("Boa tarde !");
+      }
+      else{
+        Console.WriteLine ("Boa noite !");
+      }
+    }
+    
   }
 }
