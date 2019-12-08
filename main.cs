f31d3e9fd40f16009d2803e19c83c619055977d2
ValerioZucoloto <55113492+ValerioZucoloto@users.noreply.github.com>
2019-12-08 16:17:34 -0300

    NAVIO C#

--- main.cs
+++ main.cs
@@ -2,21 +2,23 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    int x, soma=0;
+    
+    Navio x = new Navio (50, 100 , "CST", "JLG");
 
-    Console.WriteLine("Entre com um número: ");
-    x = int.Parse (Console.ReadLine());
-    soma = soma + x;
-
-    while (soma < 200) {
-      Console.WriteLine ("Entre com um número: ");
-      x = int.Parse (Console.ReadLine());
-
-      soma = soma + x;
+     Navio cst = new Navio (40, 150, "Arcelor", "GENIE");
 
 
+    if (x.MaiorCapacidade(cst)){
+      Console.WriteLine("O navio da " + x.Getnome() + " tem a maior capacidade com " + x.Getcarga_maxima ());
+    }
+    else {
+      Console.WriteLine ("O navio da " + cst.Getnome() + "da fabricante JLG" + " tem a maior capacidade com " + cst.Getcarga_maxima ());
     }
 
+    cst.Embarcar (10);
 
+    Console.WriteLine ("Capacidade atual: " + cst.Getcarga_atual());
+
+    
   }
 }
